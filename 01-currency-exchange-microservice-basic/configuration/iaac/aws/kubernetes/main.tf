7b5fb4714e2b6b12b610ee16f9db0bbf5e6518d5
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-10-23 23:18:01 +0530

    Update main.tf

--- 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -23,7 +23,7 @@ resource "aws_default_vpc" "default" {
 #}
 
 
-
+data "aws_region" "selected" {}
 #provider "kubernetes" {
  # host                   = data.aws_eks_cluster.cluster.endpoint
  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
