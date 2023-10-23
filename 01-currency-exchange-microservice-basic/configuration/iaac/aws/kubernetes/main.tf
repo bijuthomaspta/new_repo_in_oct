da3bc2a05548a08dc8a98abafce53df4527761c4
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-10-24 01:57:40 +0530

    Update main.tf

--- 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -67,21 +67,21 @@ module "my-cluster" {
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
 # ServiceAccount needs permissions to create deployments 
 # and services in default namespace
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "AdministratorAccess"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
-  }
-}
+#resource "kubernetes_cluster_role_binding" "example" {
+ # metadata {
+  #  name = "AdministratorAccess"
+  #}
+  #role_ref {
+   # api_group = "rbac.authorization.k8s.io"
+    #kind      = "ClusterRole"
+    #name      = "cluster-admin"
+  #}
+  #subject {
+   # kind      = "ServiceAccount"
+    #name      = "default"
+    #namespace = "default"
+  #}
+#}
 
 
 # Needed to set the default region
