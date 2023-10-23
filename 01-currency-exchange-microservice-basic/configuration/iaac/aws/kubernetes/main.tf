aa037ad6f507f86f61051088c2db9e6b4599357f
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-10-23 12:26:49 +0530

    Update main.tf

--- 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -23,13 +23,13 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 19.0"
+  #version                = "~> 19.0"
 }
 
 module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
-  cluster_version = "~>1.14"
+  #cluster_version = "~>1.14"
   subnet_ids       = ["subnet-03a4cf5ba5d87e41f", "subnet-02b69b4581260b7c6"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   #vpc_id          = aws_default_vpc.default.id
