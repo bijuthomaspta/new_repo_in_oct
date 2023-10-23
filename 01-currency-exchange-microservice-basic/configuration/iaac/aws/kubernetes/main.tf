5698f5456b99b02dfa5ca49ef998fa4c6bb738e4
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-10-23 22:28:07 +0530

    Update main.tf

--- 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -1,10 +1,3 @@
-# aws --version
-# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
-# Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
-
-
 terraform {
   backend "s3" {
     bucket = "mybucket" # Will be overridden from build
@@ -17,54 +10,46 @@ resource "aws_default_vpc" "default" {
 
 }
 
+# data "aws_subnet_ids" "subnets" {
+#   vpc_id = aws_default_vpc.default.id
+# }
 
-
-
-
-module "biju" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "biju1-cluster"
-  #cluster_version = "~>1.14"
-  subnet_ids       = ["subnet-0bfd3d382d079c9d8", "subnet-006dba2ac9fa506d3"] #CHANGE
-  #subnets = data.aws_subnet_ids.subnets.ids
-  #vpc_id          = aws_default_vpc.default.id
-
-  vpc_id         = "vpc-02b83871319ed6a24"
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    instance_types         = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
-  eks_managed_node_groups = {
-    green = {
-      min_size     = 3
-      max_size     = 5
-      desired_size = 3
-
-      instance_types = ["t2.micro"]
-}
-}
+data "aws_eks_cluster" "cluster" { 
+  name = "my-cluster_in_aws_eks"
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.biju.cluster_name
-  depends_on = [module.biju.cluster_name]
+data "aws_eks_cluster_auth" "cluster" { 
+  name = "my-cluster_in_aws_eks"
 }
-
-data "aws_eks_cluster_auth" "cluster1" {
-  name = module.biju.cluster_name
-  depends_on = [module.biju.cluster_name]
-}
-
 
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster1.token
- # version                = ">= 2.8"
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
+
+
+
+module "my-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster_in_aws_eks"
+#   cluster_version = "1.14"
+  subnet_ids = ["subnet-0bfd3d382d079c9d8", "subnet-006dba2ac9fa506d3"] 
+  vpc_id          = aws_default_vpc.default.id
+
+  #vpc_id         = "vpc-1234556abcdef"
+
+  eks_managed_node_groups = {
+    one = {
+      instance_type = "t2.micro"
+      max_capacity  = 3
+      desired_capacity = 3
+      min_capacity  = 1
+    }
+  }
+}
+
 
 
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
