52f40b1c1c3b52ef36499a6207442e7465deb072
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-10-24 13:18:33 +0530

    Update main.tf

--- 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -56,10 +56,13 @@ module "in28minutes-cluster" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.in28minutes-cluster.cluster_name
+  depends_on = [module.in28minutes-cluster.cluster_name]
+}
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.in28minutes-cluster.cluster_name
+  depends_on = [module.in28minutes-cluster.cluster_name]
 }
 
 
