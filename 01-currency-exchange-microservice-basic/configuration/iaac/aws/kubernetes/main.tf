09221542cfd5b5e0e4f88ca06715508355d328ce
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2023-10-24 13:45:28 +0530

    Update main.tf

--- 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ 01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -27,10 +27,10 @@ data "aws_subnets" "subnets" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   exec {
     api_version =  "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.in28minutes-cluster.cluster_name, "--role-arn", "arn:aws:iam::aws:policy/AdministratorAccess"]
+    args        = ["eks", "get-token", "--cluster-name", module.in28minutes-cluster.cluster_name]
     command     = "aws"
   }
 }
